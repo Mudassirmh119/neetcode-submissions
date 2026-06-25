06db5207117ad1be91f84da86556b735280a6c05
Muhammad Mudassir <35142924+Mudassirmh119@users.noreply.github.com>
2026-06-25 01:06:12 -0700

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,25 @@
+from collections import Counter
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pref = [1] * len(nums)
+        post = [1] * len(nums)
+        pref_prod = 1
+        post_prod = 1
+        
+        for i, num in enumerate(nums):
+            pref_prod *= num
+            pref[i] = pref_prod
+
+        for i in range(len(nums), 0, -1):        
+            post_prod *= nums[i-1]
+            post[i - 1] = post_prod
+        
+        li = []
+        for index, num in enumerate(nums):            
+            if index == 0:
+                li.append(post[index + 1])
+            elif index == len(nums) - 1:
+                li.append(pref[index - 1])
+            else:
+                li.append(pref[index - 1] * post[index + 1])        
+        return li
